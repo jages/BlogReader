625d97977861fd8c4dd25952f301035e995db34e
Jan Geske <16306035+jages@users.noreply.github.com>
2019-01-04 13:34:58 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Xml;
+
+namespace BlogReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<string> pages = new List<string>();
+            List<string> articles = new List<string>();
+            for (int i = 1; i < 37; i++) //todo: get page count via args
+            {
+                Console.WriteLine("Attempting to fetch page "+i);
+                pages.Add(getHTMLFromURL(@"https://greento100.com/page/" + i + @"/")); //todo: get page URL via args
+            }
+
+            foreach (var page in pages)
+            {
+                string tmp = page;
+                while (tmp.IndexOf(@"<article id=") > 0)
+                {
+                    int beginning = tmp.IndexOf("<article id=");
+                    int end = tmp.IndexOf(@"</article>");
+                    string article = tmp.Substring(beginning, end - beginning + 10);
+                    articles.Add(article);
+                    tmp = tmp.Substring(end + 10);
+                }
+            }
+            
+            articles.Reverse();
+            Console.WriteLine("got " + articles.Count + " articles");
+
+            int j = 5;
+            for (int i = 0; j < articles.Count;i=j)
+            {
+                j = i + 10;
+                Thread.Sleep(1100);
+                string FileName = @"C:\temp\" + DateTime.Now.ToString("yyyyMMddHHmmss") + "_articles.html";
+                Console.WriteLine("writing" +(j-i)+" to " + FileName);
+                if(j +i > articles.Count)
+                    writeListToFile(FileName, articles.GetRange(i, articles.Count -i));
+                else
+                    writeListToFile(FileName, articles.GetRange(i, j-i));    
+            }
+
+            
+            
+            
+        }
+
+        private static string getHTMLFromURL(string urlAddress)
+        {
+
+            string data = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+            }
+
+            return data;
+        }
+
+        private static void writeListToFile(string fileName,List<string> data)
+        {
+            System.IO.StreamWriter file = new StreamWriter(fileName);
+            file.Write(@"<html><body>");
+            foreach (var line in data)
+            {
+                file.Write(line);
+                file.Write(@"<hr />");
+            }
+            file.Write(@"</body></html>");
+            file.Close();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # BlogReader
 Reads the contents of a website and extracts them into seperate files
+
+A friend of mine asked me if i could help her print the contents of a Blog.
+Said Blog was created with [Wordpress](https://github.com/WordPress/WordPress), but was lacking the option to view its contents in chronological order, or to see all posts in a summary altogether. It had, however, the option to list a number of posts in a page view, so i decided to use that to my advantage and wrote a litte program.
+
+So, the attempt for this project was:
+- Get all HTML content from different pages of the blog.
+- extract the 'article' tags and content from the page HTML
+- order articles in chronological order
+- export articles as new HTML files
+- print new HTML files to PDF or ePub for viewing pleasure on ebook-reader devices
